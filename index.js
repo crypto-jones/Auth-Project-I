930b37deaec30ab6e30384f792301f8bc81e4d92
crypto-jones <integral.being@gmail.com>
2018-10-23 13:04:51 -0700

    Set up basic server

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+
+// const db = require('./data/dbConfig.js');
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
+server.get('/', (req, res) => {
+  res.send('Server is alive!');
+});
+
+const PORT = process.env.PORT || 5300;
+
+server.listen(PORT, () =>
+  console.log(`\n=== API listening on http://localhost:${PORT} === \n`)
+);
