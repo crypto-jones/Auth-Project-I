b582b5bdfc29b82b1f5432ead84468c00f22ac03
crypto-jones <integral.being@gmail.com>
2018-10-23 17:15:48 -0700

    Create login component

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Login extends Component {
+  state = {
+    username: '',
+    password: '',
+    loggedIn: false
+  };
+
+  handleChange = e => {
+    e.preventDefault();
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const login = {
+      username: this.state.username,
+      password: this.state.password
+    };
+
+    axios
+      .post('http://localhost:5300/api/login', login)
+      .then(res => {
+        console.log(res.data);
+        if (res.data.welcome !== '') {
+          this.setState({ loggedIn: true });
+          this.props.history.push('/profile');
+        } else {
+          this.props.history.push('/register');
+        }
+      })
+      .catch(err => {
+        this.props.history.push('/unauthorized');
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+          <button onClick={this.handleSubmit}>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
